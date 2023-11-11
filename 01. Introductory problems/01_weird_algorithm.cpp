b6f3b07664c6d73cad12db6775d76c58c07bc256
Omkar Bhale <omkarbhale001@gmail.com>
2023-11-11 15:06:47 +0530

    01_weird_algorithm

---
+++ 01. Introductory problems/01_weird_algorithm.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    cout << n << " ";
+    while (n != 1) {
+        if (n % 2 == 0) {
+            n = n / 2;
+        } else {
+            n = 3 * n + 1;
+        }
+        cout << n << " ";
+    }
+
+    return 0;
+}
