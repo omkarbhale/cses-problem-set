facc9fd57e23f30cd2b9d8d9cb4fedabc9a6bcbd
Omkar Bhale <omkarbhale001@gmail.com>
2023-11-11 15:14:07 +0530

    Change int to long

--- 01. Introductory problems/01_weird_algorithm.cpp
+++ 01. Introductory problems/01_weird_algorithm.cpp
@@ -1,19 +1,19 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int main() {
-    int n;
+    long n;
     cin >> n;
 
-    cout << n << " ";
+    cout << n;
     while (n != 1) {
         if (n % 2 == 0) {
             n = n / 2;
         } else {
             n = 3 * n + 1;
         }
-        cout << n << " ";
+        cout << " " << n;
     }
 
     return 0;
