274be84efb2ffc8990c30b623410d70cad04716a
Omkar Bhale <omkarbhale001@gmail.com>
2023-11-11 15:27:02 +0530

    02_missing_number

---
+++ 01. Introductory problems/02_missing_number.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    long long int n;
+    cin >> n;
+
+    long long sum = 0;
+    for (int i = 0; i < n - 1; i++) {
+        int x;
+        cin >> x;
+        sum += x;
+    }
+
+    cout << n * (n+1) / 2 - sum;
+}
---
+++ 01. Introductory problems/03_repetitions.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    string sequence;
+    cin >> sequence;
+
+    char buffer = -1;
+    long long ans = 0;
+    long long currentans = 0;
+    for (char c : sequence) {
+        if (c == buffer) {
+            currentans += 1;
+        } else {
+            ans = max(ans, currentans);
+            currentans = 1;
+            buffer = c;
+        }
+    }
+    cout << max(ans, currentans);
+}
